87a1c96affd9a8cb4551cb1f9ccc5c3f71ed0e56
Joe Doyle <joethedoyle@gmail.com>
2016-12-19 20:05:01 -0500

    Apparently it's underscore, not minus

--- config.h
+++ config.h
@@ -107,15 +107,15 @@ static Mousekey mshortcuts[] = {
 #define MODKEY Mod1Mask
 
 static Shortcut shortcuts[] = {
-	/* mask                 keysym          function        argument */
-	{ ControlMask,          XK_Print,       toggleprinter,  {.i =  0} },
-	{ ShiftMask,            XK_Print,       printscreen,    {.i =  0} },
-	{ XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
-	{ MODKEY|ShiftMask,     XK_plus,        xzoom,          {.i = +1} },
-	{ MODKEY|ShiftMask,     XK_minus,       xzoom,          {.i = -1} },
-	{ ControlMask|ShiftMask,XK_C,           selpaste,       {.i =  0} },
-	{ ControlMask|ShiftMask,XK_V,           clippaste,      {.i =  0} },
-	{ MODKEY,               XK_Num_Lock,    numlock,        {.i =  0} },
+	/* mask                  keysym          function        argument */
+	{ ControlMask,           XK_Print,       toggleprinter,  {.i =  0} },
+	{ ShiftMask,             XK_Print,       printscreen,    {.i =  0} },
+	{ XK_ANY_MOD,            XK_Print,       printsel,       {.i =  0} },
+	{ ControlMask|ShiftMask, XK_plus,        xzoom,          {.i = +1} },
+	{ ControlMask|ShiftMask, XK_underscore,  xzoom,          {.i = -1} },
+	{ ControlMask|ShiftMask, XK_C,           selpaste,       {.i =  0} },
+	{ ControlMask|ShiftMask, XK_V,           clippaste,      {.i =  0} },
+	{ MODKEY,                XK_Num_Lock,    numlock,        {.i =  0} },
 };
 
 /*
